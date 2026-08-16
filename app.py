d7dad1128a1c0210c2f0611dacd90ddb9e13a140
Samir1-T <samirturkay@icloud.com>
2026-08-17 00:34:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -71,32 +71,31 @@ if pathology == "Keratoconus":
 elif pathology == "Cataracts":
 
     st.header("Cataracts")
-    tab1, tab2, tab3(
-        ["Overview","Symptoms", "Signs", "Management", "Images"]
+    tab1, tab2, tab3,tab4(
+        ["Overview","Symptoms", "Signs", "Management"]
     )
-    with tab 1:
-    st.subheader("Cataracts")
-    st.write (" A common eye condition in which the lens turns cloudy")
-
-   
-
-    st.subheader("Symptoms")
-    st.write("- Gradual reduction in vision")
-    st.write("- Glare")
-    st.write("- Reduced contrast")
-    st.write("- Difficulty driving at night")
-    st.subheader("Different types of cataracts")
-    st.write( "- Nuclear cataracts: Forms at the centre of the lens; The nucleaus")
-    st.write(" - Cortical cataracts: Starts the edge of the lens, the cortex. Appear as wedge-shaped spokes")
-    st.write(" - Posterior subcapsular: Form at the posterior lens")
-
-    st.subheader("Clinical Signs")
-    st.write("- Lens opacity")
-    st.write("- Reduced red reflex")
-    st.write("- Reduced visual acuity")
+    with tab1:
+        st.subheader("Cataracts")
+        st.write (" A common eye condition in which the lens turns cloudy")
+        st.write ("#### Different types of cataracts")
+        st.write (" - Nuclear")
+        st.write (" - Cortical")
+        st.write (" - Posterior subcapsular")
     
-    st.subheader("Management")
-    st.write(" ##### Referral is only required; ")
-    st.write (" 1. If vision is worse than 6/12 in the affected eye")
-    st.write (" 2. Visual symptoms e.g Glare, loss of colour/contrast ,anisometropia")
-    st.write (" 3. Affecting daily tasks/living independantly")
+    with tab2:
+        st.subheader("Symptoms")
+        st.write("- Gradual reduction in vision")
+        st.write("- Glare")
+        st.write("- Reduced contrast")
+        st.write("- Difficulty driving at night")
+    with tab3:
+        st.subheader ("Clinical signs")
+        st.write("- Lens opacity")
+        st.write("- Reduced red reflex")
+        st.write("- Reduced visual acuity")
+    with tab4:
+        st.subheader("Management")
+        st.write(" ##### Referral is only required; ")
+        st.write (" 1. If vision is worse than 6/12 in the affected eye")
+        st.write (" 2. Visual symptoms e.g Glare, loss of colour/contrast ,anisometropia")
+        st.write (" 3. Affecting daily tasks/living independantly")
