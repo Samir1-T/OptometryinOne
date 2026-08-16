facfe8c0011a25dc3c53897147df914965154528
Samir1-T <samirturkay@icloud.com>
2026-08-16 23:37:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ elif pathology == "Cataract":
     st.write("- Reduced contrast")
     st.write("- Difficulty driving at night")
     st.subheader("Different types of cataracts")
-    st.write(- "Nuclear cataracts; Forms in the centre of the lens; The nucleaus. Usually turn yellow or brown depending of the severity")
+    st.write( "- Nuclear cataracts; Forms in the centre of the lens; The nucleaus. Usually turn yellow or brown depending of the severity")
 
     st.subheader("Clinical Signs")
     st.write("- Lens opacity")
