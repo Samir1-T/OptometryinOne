ae7dff0cab774721262b08ac568214dca19c3474
Samir1-T <samirturkay@icloud.com>
2026-08-16 23:55:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ elif pathology == "Cataracts":
     st.write("- Reduced visual acuity")
     
     st.subheader("Management")
-    st.write(" ##### Referral is only required if vision is worse than 6/12 in the affected eye")
+    st.write(" ##### Referral is only required if; ")
     st.write (" 1. If vision is worse than 6/12 in the affected eye")
     st.write (" 2. Visual symptoms e.g Glare, loss of colour/contrast ,anisometropia")
     st.write (" 3. Affecting daily tasks/living independantly")
