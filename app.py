b0641e86e6ac5b60333c8299c97cda1d501a3081
Samir1-T <samirturkay@icloud.com>
2026-08-18 16:58:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ elif pathology == "Retinal Vein Occlusion":
         )
         st.write (" - Sudden or gradual loss of vision")
         st.write (" - Painless")
-        st.markdown ("<u>BRVO </u"
+        st.markdown ("<u>BRVO </u",
                     unsafe_allow+html=True
                     )
         st.write (" - Symptoms vary depending on severity and location e.g asymptomatic if BRVO occurs at the periphery")
